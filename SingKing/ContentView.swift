2ed04bcd01a80e84e3ee487116b5ada8a08b92a3
Ehsan Askari <>
2022-10-09 17:47:32 +0300

    Initial

---
+++ SingKing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SingKing
+//
+//  Created by Ehsan Askari on 10/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ SingKing/SingKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingKingApp.swift
+//  SingKing
+//
+//  Created by Ehsan Askari on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SingKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
